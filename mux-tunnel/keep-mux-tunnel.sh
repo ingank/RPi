040d080aacb711e3a33a91f6b6c2c906cc169da5
Ingolf Ankert <git@ingank.net>
2020-05-13 23:18:45 +0200

    Create keep-mux-tunnel.sh

---
+++ mux-tunnel/keep-mux-tunnel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true
+do
+    echo -e "\n\n---" | tee -a ./tunnel.log
+    date | tee -a ./tunnel.log
+    echo -e "---\n\n" | tee -a ./tunnel.log
+    ssh -6 -v \
+        -R [::1]:2222:[::1]:2222 \
+        -o ServerAliveInterval=180 \
+        -o ServerAliveCountMax=3 \
+        foo@server.domain.tld \
+        2>>./tunnel.log
+	sleep  180
+done
