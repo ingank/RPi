70b612e4c75f3c00853dac8f69762be2eb99fd1f
Ingolf Ankert <git@ingank.net>
2020-05-13 23:33:15 +0200

    Update keep-mux-tunnel.sh

--- mux-tunnel/keep-mux-tunnel.sh
+++ mux-tunnel/keep-mux-tunnel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# hier wird der Name des externen V-Servers eingetragen:
+server=server.domain.tld
+
 while true
 do
     echo -e "\n\n---" | tee -a ./tunnel.log
@@ -9,7 +12,7 @@ do
         -R [::1]:2222:[::1]:2222 \
         -o ServerAliveInterval=180 \
         -o ServerAliveCountMax=3 \
-        tunnel@server.domain.tld \
+        tunnel@${server} \
         2>>./tunnel.log
 	sleep  180
 done
